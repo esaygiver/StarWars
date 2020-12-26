4bbd5c5b3ea7f6f981289df6c83c250da80b309b
esaygiver <saygivere@gmail.com>
2020-12-26 14:05:34 +0300

    first commit

---
+++ StarWars/SelectionScreen.swift
@@ -0,0 +1,30 @@
+//
+//  SelectionScreen.swift
+//  StarWars
+//
+//  Created by admin on 2.12.2020.
+//  Copyright © 2020 esaygiver. All rights reserved.
+//
+
+import UIKit
+
+class SelectionScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
